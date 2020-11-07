6f3060b5059a0ecfb415f5f074c7dc9e9ebf78ab
Editux <43064329+Editux@users.noreply.github.com>
2020-11-07 18:09:59 +0200

    Update Main.rb

--- Task3.1/Main.rb
+++ Task3.1/Main.rb
@@ -4,15 +4,16 @@ require 'digest'
 require 'openssl'
 require './aes'
 
+#calling the object
 object= Aes. new
-
+#digesting the key
 password = Digest::SHA256.digest('Nixnogen')
 iv       = 'a2xhcgAAAAAAAAAA'
-buf      = "Here is some data for the encrypt"
-
-enc =object.encrypt(password,iv,buf)
+data      = "Here is some data for the encrypt"
+#Calling out methods from Aes class to encrypt and decrypt the cipher
+enc =object.encrypt(password,iv,data)
 dec =object.decrypt(password,iv,enc)
-
+#Outputing to console
 puts "encrypt length: #{enc.length}"
 puts "encrypt in Base64: " + enc
 puts "decrypt all: " + dec
